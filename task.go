087646711058bdb7ba55e473fc807fb2bac07fa4
dima_simakin@mail.ru <dima_simakin@mail.ru>
2023-11-10 00:36:41 +0300

    Changed calculator

--- task.go
+++ task.go
@@ -4,30 +4,29 @@ import "fmt"
 
 func main() {
 
-	var expression string
 	var operator string
 	var number1 int8
 	var number2 int8
 
-	fmt.Print("Enter expression : ")
-	fmt.Scanln(&expression)
+	fmt.Print("expression")
+	fmt.Scanln(&number1, &operator, &number2)
 
 	switch operator {
 
 	case "+":
-		fmt.Printf("%.3f %s %.3f = %.3f", number1, operator, number2, number1+number2)
+		fmt.Printf("%d %s %d = %d", number1, operator, number2, number1+number2)
 
 	case "-":
-		fmt.Printf("%.3f %s %.3f = %.3f", number1, operator, number2, number1-number2)
+		fmt.Printf("%d %s %d = %d", number1, operator, number2, number1-number2)
 
 	case "*":
-		fmt.Printf("%.3f %s %.3f = %.3f", number1, operator, number2, number1*number2)
+		fmt.Printf("%d %s %d = %d", number1, operator, number2, number1*number2)
 
 	case "/":
 		if number2 == 0.0 {
 			fmt.Println("Divide by Zero Situation")
 		} else {
-			fmt.Printf("%.3f %s %f = %.3f", number1, operator, number2, number1/number2)
+			fmt.Printf("%d %s %d = %d", number1, operator, number2, number1/number2)
 		}
 	default:
 		fmt.Println("Invalid Operator")
